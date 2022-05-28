016195672436968de828fedff39b959b7b3f38bb
mloaizau <mloaiza.urrutia@gmail.com>
2022-05-28 11:41:22 -0400

    limpieza de codigo

--- src/app/auth/registro/mensaje-confirmacion/mensaje-confirmacion/mensaje-confirmacion.component.ts
+++ src/app/auth/registro/mensaje-confirmacion/mensaje-confirmacion/mensaje-confirmacion.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mensaje-confirmacion',
@@ -8,9 +7,7 @@ import { Router } from '@angular/router';
 })
 export class MensajeConfirmacionComponent implements OnInit {
 
-  constructor(
-    private router: Router
-  ) { }
+  constructor() { }
 
   ngOnInit() {}
 

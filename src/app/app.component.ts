7cfdb0be0756b67ea0a6440d4b1d24426cd89c3a
mloaizau <mloaiza.urrutia@gmail.com>
2022-05-29 12:03:15 -0400

    enrutamiento del menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { SessionService } from "./services/session.service";
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  public session: any;
+
   constructor(
-    public menuCtrl: MenuController
-  ) {}
+    public menuCtrl: MenuController,
+    private _session: SessionService,
+    private route: Router
+  ) {
+    this.obtenerSession();
+  }
 
   closeMenu() {
     this.menuCtrl.close();
+  }
+
+  async obtenerSession(){
+    this.session = await this._session.get("sessionActive");
+  }
+
+  async ionViewWillEnter(){
+    if(!this.session){
+      this.obtenerSession();
+    }
+  }
+
+  enrutamiento(url){
+    this.route.navigate(["/home/tabs/"+url]);
+    this.closeMenu();
   }
 }

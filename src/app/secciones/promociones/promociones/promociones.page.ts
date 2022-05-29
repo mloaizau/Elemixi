d38e9e673fee008b52d233ba37f0a15e26a1034b
mloaizau <mloaiza.urrutia@gmail.com>
2022-05-29 14:29:26 -0400

    se agrega maqueta y contendio de catalogo

--- src/app/secciones/promociones/promociones/promociones.page.ts
+++ src/app/secciones/promociones/promociones/promociones.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StoreService } from '../../../services/store.service';
 
 @Component({
   selector: 'app-promo',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class PromocionesPage {
 
-  constructor() {}
+  public productos: any;
+
+  constructor(
+    private _storeService: StoreService
+  ) { }
+
+  async getStore(){
+    this.productos = await this._storeService.getStore();
+    console.log(this.productos);
+  }
+
+  async ionViewWillEnter(){
+    if(!this.productos){
+      this.getStore();
+    }
+  }
 
 }
